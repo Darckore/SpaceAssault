e185b45c9b88ee73cc07191a05a6c514fa5254b4
darckore <darckore@gmail.com>
2022-03-30 23:38:10 +0300

    direct3d garbage

--- core/src/gfx/renderer.cpp
+++ core/src/gfx/renderer.cpp
@@ -4,11 +4,11 @@
 #include "core/win_includes.hpp"
 #include "assets/sprite.hpp"
 
-#include <d2d1helper.h>
-#include <dwrite.h>
-#include <wincodec.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
 
-#pragma comment(lib, "d2d1")
+#pragma comment (lib, "d3d12.lib")
+#pragma comment (lib, "dxgi.lib")
 
 namespace engine::graphics
 {
@@ -16,45 +16,19 @@ namespace engine::graphics
 
   namespace detail
   {
-    static inline auto wnd_size(const window& wnd) noexcept
-    {
-      auto [w, h] = wnd.size();
-      return D2D1::SizeU(static_cast<UINT32>(w), static_cast<UINT32>(h));
-    }
-
-    template <typename I>
-    void release(I** resource) noexcept
-    {
-      if (*resource)
-      {
-        (*resource)->Release();
-        (*resource) = nullptr;
-      }
-    }
-
     class bitmap
     {
     public:
-      using value_type = ID2D1Bitmap*;
+      using value_type = void*;
 
     public:
       CLASS_SPECIALS_NONE(bitmap);
 
-      ~bitmap() noexcept
-      {
-        release(&m_bitmap);
-      }
+      ~bitmap() noexcept = default;
 
-      bitmap(ID2D1RenderTarget* target, const sprite& s) noexcept
+      bitmap(void* target, const sprite& s) noexcept
       {
-        auto src = s.data();
-        
-        const auto sz     = D2D1::SizeU(s.width(), s.height());
-        const auto format = D2D1::PixelFormat(DXGI_FORMAT_B8G8R8A8_UNORM, D2D1_ALPHA_MODE_IGNORE);
-        const auto props  = D2D1::BitmapProperties(format);
-        const auto pitch  = s.width() * 4;
-        const auto res    = target->CreateBitmap(sz, src, pitch, &props, &m_bitmap);
-        utils::unused(res);
+        utils::unused(target, s);
       }
 
       explicit operator bool() const noexcept
@@ -118,26 +92,26 @@ namespace engine::graphics
 
   renderer::operator bool() const noexcept
   {
-    return static_cast<bool>(m_target);
+    return static_cast<bool>(m_device);
   }
 
   // Public members
 
   void renderer::resize() noexcept
   {
-    auto size = detail::wnd_size(m_wnd);
-    m_target->Resize(size);
+    //auto size = detail::wnd_size(m_wnd);
+    //m_target->Resize(size);
   }
 
   void renderer::init_drawing() noexcept
   {
-    m_target->BeginDraw();
-    m_target->Clear(D2D1::ColorF{ D2D1::ColorF::AntiqueWhite });
+    //m_target->BeginDraw();
+    //m_target->Clear(D2D1::ColorF{ D2D1::ColorF::AntiqueWhite });
   }
   void renderer::end_drawing() noexcept
   {
-    auto res = m_target->EndDraw();
-    utils::unused(res);
+    //auto res = m_target->EndDraw();
+    //utils::unused(res);
   }
 
   // Draw stuff
@@ -157,53 +131,202 @@ namespace engine::graphics
     }
 
     utils::unused(pos, dir);
-    //ID2D1SolidColorBrush* brush{};
-    //m_target->CreateSolidColorBrush(D2D1::ColorF{ D2D1::ColorF::DarkBlue }, &brush);
-    //auto p1 = viewport_to_screen(v1);
-    //auto p2 = viewport_to_screen(v2);
-    //m_target->DrawLine(D2D1::Point2F(p1[0], p1[1]),
-    //                   D2D1::Point2F(p2[0], p2[1]),
-    //                   brush,
-    //                   5.0f);
-    //
-    //detail::release(&brush);
   }
 
   // Private members
 
+  bool renderer::enable_dbg() noexcept
+  {
+  #ifndef NDEBUG
+    com_ptr<ID3D12Debug> dbg;
+    const auto res = D3D12GetDebugInterface(IID_PPV_ARGS(&dbg));
+    if (FAILED(res))
+    {
+      // todo: error
+      return false;
+    }
+    dbg->EnableDebugLayer();
+  #endif
+    return true;
+  }
+  bool renderer::create_adapter() noexcept
+  {
+    constexpr std::uint32_t flags =
+  #ifndef NDEBUG
+      DXGI_CREATE_FACTORY_DEBUG;
+  #else
+      0;
+  #endif
+
+    auto res = CreateDXGIFactory2(flags, IID_PPV_ARGS(&m_factory));
+    if (FAILED(res))
+    {
+      // todo: error
+      return false;
+    }
+
+    auto vmem = 0ull;
+    com_ptr<IDXGIAdapter1> adapter{};
+    auto checkAdapter = [&vmem, &adapter](const DXGI_ADAPTER_DESC1& descr)
+    {
+      const auto isHW   = (descr.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) == 0;
+      const auto devRes = D3D12CreateDevice(adapter.Get(),
+                                            D3D_FEATURE_LEVEL_11_0,
+                                            __uuidof(ID3D12Device),
+                                            nullptr);
+      const auto devOk = isHW && SUCCEEDED(devRes);
+      return devOk && descr.DedicatedVideoMemory > vmem;
+    };
+
+    for (auto idx = 0u; m_factory->EnumAdapters1(idx, &adapter) != DXGI_ERROR_NOT_FOUND; ++idx)
+    {
+      DXGI_ADAPTER_DESC1 adapterDescr;
+      adapter->GetDesc1(&adapterDescr);
+
+      if (checkAdapter(adapterDescr))
+      {
+        vmem = adapterDescr.DedicatedVideoMemory;
+        res = adapter.As(&m_adapter);
+      }
+
+      if (FAILED(res))
+      {
+        // todo: error
+        return false;
+      }
+    }
+
+    return true;
+  }
+  bool renderer::create_device() noexcept
+  {
+    auto res = D3D12CreateDevice(m_adapter.Get(),
+                                 D3D_FEATURE_LEVEL_11_0,
+                                 IID_PPV_ARGS(&m_device));
+    if (FAILED(res))
+    {
+      // todo: error
+      return false;
+    }
+
+  #ifndef NDEBUG
+    com_ptr<ID3D12InfoQueue> infoQueue;
+    if (SUCCEEDED(m_device.As(&infoQueue)))
+    {
+      infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, TRUE);
+      infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, TRUE);
+      infoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, TRUE);
+
+      D3D12_MESSAGE_SEVERITY severities[] { D3D12_MESSAGE_SEVERITY_INFO };
+      D3D12_MESSAGE_ID ids[] {
+          D3D12_MESSAGE_ID_CLEARRENDERTARGETVIEW_MISMATCHINGCLEARVALUE,
+          D3D12_MESSAGE_ID_MAP_INVALID_NULLRANGE,
+          D3D12_MESSAGE_ID_UNMAP_INVALID_NULLRANGE
+      };
+
+      D3D12_INFO_QUEUE_FILTER filter{};
+      filter.DenyList.NumSeverities = _countof(severities);
+      filter.DenyList.pSeverityList = severities;
+      filter.DenyList.NumIDs        = _countof(ids);
+      filter.DenyList.pIDList       = ids;
+      infoQueue->PushStorageFilter(&filter);
+    }
+  #endif
+
+    return true;
+  }
+  bool renderer::create_cmd_queue() noexcept
+  {
+    D3D12_COMMAND_QUEUE_DESC desc{};
+    desc.Type     = D3D12_COMMAND_LIST_TYPE_DIRECT;
+    desc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+    desc.Flags    = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    desc.NodeMask = 0;
+    return SUCCEEDED(m_device->CreateCommandQueue(&desc, IID_PPV_ARGS(&m_cmdQueue)));
+  }
+  bool renderer::create_swapchain() noexcept
+  {
+    auto [w, h] = m_wnd.size();
+    DXGI_SWAP_CHAIN_DESC1 desc;
+    desc.Width       = static_cast<unsigned>(w);
+    desc.Height      = static_cast<unsigned>(h);
+    desc.Format      = DXGI_FORMAT_R8G8B8A8_UNORM;
+    desc.Stereo      = FALSE;
+    desc.SampleDesc  = { 1, 0 };
+    desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    desc.BufferCount = bufCnt;
+    desc.Scaling     = DXGI_SCALING_STRETCH;
+    desc.SwapEffect  = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    desc.AlphaMode   = DXGI_ALPHA_MODE_STRAIGHT;
+    desc.Flags       = 0;
+
+
+    //ThrowIfFailed(dxgiFactory4->CreateSwapChainForHwnd(
+
+
+    //    commandQueue.Get(),
+
+
+    //    hWnd,
+
+
+    //    &swapChainDesc,
+
+
+    //    nullptr,
+
+
+    //    nullptr,
+
+
+    //    &swapChain1));
+
+ 
+
+
+    //// Disable the Alt+Enter fullscreen toggle feature. Switching to fullscreen
+
+
+    //// will be handled manually.
+
+
+    //ThrowIfFailed(dxgiFactory4->MakeWindowAssociation(hWnd, DXGI_MWA_NO_ALT_ENTER));
+
+ 
+
+
+    //ThrowIfFailed(swapChain1.As(&dxgiSwapChain4));
+
+ 
+
+
+    //return dxgiSwapChain4;
+
+
+    return true;
+  }
+
   void renderer::init() noexcept
   {
-    if (!m_wnd)
-      return;
-
-    auto res = D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_factory);
-    if (FAILED(res))
+    const bool success
     {
-      // todo: error
-      release();
+         static_cast<bool>(m_wnd)
+      && enable_dbg()
+      && create_adapter()
+      && create_device()
+      && create_cmd_queue()
+      && create_swapchain()
+    };
+
+    if (!success)
+    {
+      m_device = nullptr;
       return;
     }
-
-    auto wndHandle = m_wnd.handle<HWND>();
-    auto size = detail::wnd_size(m_wnd);
-    res = m_factory->CreateHwndRenderTarget(D2D1::RenderTargetProperties(),
-                                            D2D1::HwndRenderTargetProperties(wndHandle, size),
-                                            &m_target);
-
-    if (FAILED(res))
-    {
-      // todo: error
-      release();
-      return;
-    }
-
-    m_target->SetTransform(D2D1::Matrix3x2F::Identity());
   }
   void renderer::release() noexcept
   {
     m_bitmapCache.clear();
-    detail::release(&m_factory);
-    detail::release(&m_target);
   }
 
   renderer::pixel_type renderer::viewport_to_screen(const point_type& v) const noexcept
@@ -223,7 +346,7 @@ namespace engine::graphics
       return item->second;
     }
 
-    auto newitem = m_bitmapCache.try_emplace(key, m_target, s);
+    auto newitem = m_bitmapCache.try_emplace(key, nullptr, s);
     return newitem.first->second;
   }
 }
